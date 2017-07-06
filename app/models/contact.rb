cd71c74489540b628358f623a6bcebf15b0a73b3
Milton Quirino <miltonquirino.br@gmail.com>
2017-07-06 14:47:13 -0300

    Mapeado, um contato tem muitos telefones

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,26 @@
 class Contact < ApplicationRecord
 
+  #associacoes
   belongs_to :kind, optional: true
+  has_many :phones
+
+  def as_json(options={})
+    h = super(options)
+    h[:birthdate] = (I18n.l(self.birthdate) unless self.birthdate.blank?)
+    h
+  end
+
+  # def birthdate_br
+  #   I18n.l(self.birthdate) unless self.birthdate.blank?
+  # end
+
+  # def to_br
+  #   {
+  #       name: self.name,
+  #       email: self.email,
+  #       birthdate: (I18n.l(self.birthdate) unless self.birthdate.blank?)
+  #   }
+  # end
 
   # def author
   #   "Milton Quirino"
@@ -17,5 +37,10 @@ class Contact < ApplicationRecord
   #         include: {kind: { only: :description }}
   #     )
   # end
+
+  # def hello
+  #   I18n.t('hello')
+  # end
+
 
 end
