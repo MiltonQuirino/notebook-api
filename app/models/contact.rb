263904517daf9c5347b9904bea58ba896e0bc891
Milton Quirino <miltonquirino.br@gmail.com>
2017-07-06 10:39:07 -0300

    add reference return json contact and kind

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,21 +1,21 @@
 class Contact < ApplicationRecord
 
-  belongs_to :kind
+  belongs_to :kind, optional: true
 
-  def author
-    "Milton Quirino"
-  end
-
-  def kind_description
-    self.kind.description
-  end
-
-  def as_json(options={})
-      super(
-          root: true,
-          methods: [:author, :kind_description],
-          include: {kind: { only: :description }}
-      )
-  end
+  # def author
+  #   "Milton Quirino"
+  # end
+  #
+  # def kind_description
+  #   self.kind.description
+  # end
+  #
+  # def as_json(options={})
+  #     super(
+  #         root: true,
+  #         methods: [:author, :kind_description],
+  #         include: {kind: { only: :description }}
+  #     )
+  # end
 
 end
