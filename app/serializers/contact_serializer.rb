720b1f5af5820588554c9e2649cf035d9a67fc5c
Milton Quirino <miltonquirino.br@gmail.com>
2017-07-11 10:33:40 -0300

    serialize

---
+++ app/serializers/contact_serializer.rb
@@ -0,0 +1,21 @@
+class ContactSerializer < ActiveModel::Serializer
+  attributes :id, :name, :email, :birthdate
+
+  belongs_to :kind
+  has_many :phones
+  has_one :address
+
+end
+
+# meta do
+#   {autor: "milton"}
+# end
+
+def attributes(*args)
+  h = super(*args)
+
+  #pt-BR h[:birthdate] = (I18n.l(object.birthdate) unless object.birthdate.blank?)
+
+  h[:birthdate] = object.birthdate.to_time.iso8601 unless object.birthdate.blank?
+  h
+end
